3b1d6cfd916278e195d90b11e44aecef29e9af8c
Franck Rupin <franck.rupin@gmail.com>
2016-06-01 22:30:07 -0400

    added structures for packets

---
+++ lib/jconverter.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <jansson.h>
+
+#include "uhp.h"
+
+static json_t*
+keep2json(void *pkt)
+{
+	json_t *obj;
+	return obj;
+}
+
+static json_t*
+hello2json(void *pkt)
+{
+	json_t *obj;
+	return obj;
+}
+
+static json_t*
+ack2json(void *pkt)
+{
+	json_t *obj;
+	return obj;
+}
+
+static json_t*
+bye2json(void *pkt)
+{
+	json_t *obj;
+	return obj;
+}
+
+json_t *
+pkt2json(struct hdr_pkt *pkt)
+{
+	switch(pkt->id){
+		case KEEPALIVE: 
+                        return keep2json(pkt);
+		case HELLO:	
+                        return hello2json(pkt);
+		case ACK:	
+                        return ack2json(pkt);  
+		case BYE:	
+                        return bye2json(pkt);
+                default:        
+                        return NULL;
+	}
+}
+
--- lib/uhp.h
+++ lib/uhp.h
@@ -1,15 +1,59 @@
 #ifndef UHP_H
 #define UHP_H
 
+#include <stdint.h>
 #include <openssl/ssl.h>
 #include <openssl/rsa.h>
 #include <openssl/x509.h>
 #include <openssl/evp.h>
+#include <jansson.h>
 
 #include "error.h"
 
 #define UHPPORT	4000
-int run_udp();
-int run_ux();
 
-#endif /* UHP_H_ */
+#define KEEPALIVE_TAG   "KEEP_ALIVE"
+#define HELLO_TAG       "HELLO"
+#define ACK_TAG         "ACK"
+#define BYE_TAG         "BYE"
+
+enum{
+	KEEPALIVE,
+	HELLO,
+	ACK,
+	BYE
+};
+
+struct hdr_pkt{
+	int	id;
+	union{ 
+		char 		*msg;
+		struct hello_pl *hpl;
+		struct ack_pl   *apl;
+		struct bye_pl	*bpl;
+	}pl;
+};
+
+struct hello_pl{
+        char            tag[6];
+        uint32_t        rand;
+        time_t          timestamp;
+};
+
+struct ack_pl;
+struct bye_pl;
+
+struct hdr_pkt  *new_keepalive();
+struct hdr_pkt  *new_hello();
+struct hdr_pkt  *new_ack();
+struct hdr_pkt  *new_bye();
+void            free_pkt(struct hdr_pkt*);
+
+json_t          *pkt2json(struct hdr_pkt*);
+struct hdr_pkt  *json2pkt(json_t*);
+void            read_pkt(struct hdr_pkt*);
+
+int             run_udp();
+int             run_ux();
+
+#endif /* UHP_H */
