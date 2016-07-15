763a3cc67c8da43df6fd27efb738cf841476d76c
0xFranckx0 <franck.rupin@gmail.com>
2016-07-15 14:36:45 -0400

    added dev random and urandom support

--- include/uhp_utils.c
+++ include/uhp_utils.c
@@ -29,28 +29,70 @@
 
 #include "uhp_utils.h"
 
+#if defined(HAVE_DEVRAND) || defined(HAVE_DEVURAND)
+static int 	__uhp_rand(const char*, unsigned char*, int);
+#endif /* HAVE_DEVRAND... */
+
+#if defined(HAVE_DEVRAND) || defined(HAVE_DEVURAND)
+int
+__uhp_rand(const char *device, unsigned char *byte, int blen)
+{
+    int fd;
+    ssize_t noc;
+
+    fd = open(device, O_RDONLY);
+    if (fd < 0) {
+	error_sys_set(__func__, "open");
+	return -1;
+    }
+    noc = read(fd, (void *) byte, blen);
+    if (noc < 0) {
+	error_sys_set(__func__, "read");
+	(void) close(fd);
+	return -1;
+    }
+    if (noc != blen) {
+	error_set(__func__, "read %zd != %zu bytes", noc, blen);
+	(void) close(fd);
+	return -1;
+    }
+    (void) close(fd);
+    return 0;
+}
+#endif /* HAVE_DEVRAND... */
 
 unsigned char*
 uhp_rand(int s)
 {
 	unsigned char *rands;
+	int nb;
 	rands = malloc(sizeof(rand)*s);
 	if (rands == NULL){
 		perror("malloc()");
 		goto error;
 	}
 	
-	if( !(RAND_bytes(rands, s))) {
+	if( (nb = (RAND_bytes(rands, s))) < 0 ) {
 		perror("ERROR: call to RAND_pseudo_bytes() failed\n");
+#ifdef HAVE_DEVURAND
+		else if ( (nb = __uhp_rand("/dev/urandom", rand, s)) > 0 )
+			goto out;
+#endif /* HAVE_DEVURAND... */
+#ifdef HAVE_DEVRAND
+		else if ( (nb = __uhp_rand("/dev/random", rand, s)) > 0 )
+			goto out;
+#endif /* HAVE_DEVRAND... */	
+	}
+	 if (nb < 0 )
 		goto error;
-	} 
-
-	return rands;
-	
+		
 error:
 	if (rands != NULL)
 		free(rands);
-	retun NULL;
+	return NULL;
+
+out:
+	return rands;
 }
 
 int
