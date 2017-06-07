33b33f3ba88d8e859e571862511828b068b79a13
0xFranckx0 <franck.rupin@gmail.com>
2017-06-06 20:59:10 -0400

    Implement init function

--- include/uhp_protocol.c
+++ include/uhp_protocol.c
@@ -1,5 +1,13 @@
 #include "uhp.h"
 
+void 
+init_table(struct transaction **table, int s)
+{
+        int i;
+        for (i = 0; i < s; i++)
+                *(table+i) = NULL;
+}
+
 void
 next_operation(struct transaction **table, struct punch_msg *msg, int port)
 {
