80571dad5435edd3c33268fdd1a79c32ccbe8c40
0xFranckx0 <franck.rupin@gmail.com>
2017-07-12 21:23:01 -0400

    add a function for converting random number to int always positif

--- src/proto_test.c
+++ src/proto_test.c
@@ -10,44 +10,55 @@ main()
         struct punch_msg *pm = NULL;
         int i, err;
         uint32_t id;
-        unsigned char buf[32], **b;
+        unsigned char buf[MAX_BYTES];
+        uint16_t  **b;
+        uint8_t bu[MAX_BYTES];
         char *e_str;
+    	int res = 0;
 
         init_table(transac_table, MAX_PORT);
-
+/*
         for (i = 0; i < MAX_PORT; i++) {
                 if ( transac_table[i] == NULL)
                         printf("NOT INITIALIZED\n");
         }
-
+*/
         //next_operation(transac_table, &pm, 2);
+        RAND_bytes(bu, MAX_BYTES);
+	for (int i = 0; i < MAX_BYTES; ++i) { 
+#if BYTE_ORDER == BIG_ENDIAN
+		    res |= (bu[i] & 1) << (7 - i);
+#elif BYTE_ORDER == LITTLE_ENDIAN
+		    res |= (bu[i] & 1) << (i);
+#endif
+	}
+	printf("%d\n",res);
+     //   pm = next_msg(transac_table, 2222);
+//        printf("%d", pm->punchid);
         /*
-        pm = next_msg(transac_table, 2222);
-        printf("TAG = %d\nPUNCHID = %ld\nEPOCH = %d\nSEQ = %d\n",
-                        pm->tag, pm->punchid, pm->epoch, pm->count);
-        del_punch_msg(pm);
-        */
-        //memset(buf,1,32);
-        new_transaction(transac_table, 2222);
-        for (i = 0; i < 32; i++) 
-                fprintf(stdout,"%i",transac_table[2222]->punchid[i]);
+        for (i = 0; i < MAX_BYTES ; i++)
+                printf("%i", pm->punchid[i]);
         printf("\n");        
-        /*
-        printf("punchid = %s\nType = %d\nOrigin = %d\nMaster = %d\n ",
-                transac_table[2222]->punchid, transac_table[2222]->type,
-                transac_table[2222]->origin, transac_table[2222]->master);
-       */
-       // printf("BUF: %c\n", transac_table[2222]->punchid[0]);
-       b64_encode((const unsigned char *)transac_table[2222]->punchid,
-                       32,&e_str);
-       if (e_str != NULL)
+        */
+/*        b64_encode((const uint16_t *)pm->punchid,MAX_BYTES,&e_str);
+        if (e_str != NULL)
+               printf("%s\n", e_str);
+        new_transaction(transac_table, 2222);
+        memcpy(transac_table[2222]->punchid,  pm->punchid, MAX_BYTES);
+  */      //RAND_bytes(transac_table[2222]->punchid,32);
+/*        b64_encode((const unsigned char*)transac_table[2222]->punchid,MAX_BYTES,&e_str);
+        if (e_str != NULL)
                printf("%s\n", e_str);
 
-        b64_decode(e_str, &buf, 32);
-        for (i = 0; i < 32; i++) 
-                fprintf(stdout,"%i", buf[i]);
+        b64_decode(e_str, b, MAX_BYTES);
+        fprintf(stdout,"%d", *b);
+  */      /*
+        for (i = 0; i < MAX_BYTES; i++) 
+                fprintf(stdout,"%i", b[i]);
         printf("\n");        
-        del_transaction(transac_table[2222]);        
-
+        */
+    /*    del_transaction(transac_table[2222]);        
+        del_punch_msg(pm);
+*/
         return 0;
 }
